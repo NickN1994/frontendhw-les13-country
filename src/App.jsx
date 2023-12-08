2829e95038d0c86010b4892025a8cea6b17b71f0
NickN1994 <nickneggers@hotmail.com>
2023-12-08 16:07:55 +0100

    begin gemaakt aan opdracht 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import worldmap from "./assets/world_map.png";
 import axios from "axios";
 import CountryTile from "./Components/CountryTile.jsx";
 import color from "./helpers/color.js";
-import countryTile from "./Components/CountryTile.jsx";
 
 function App() {
     const [countries, setCountries] = React.useState([]);
+    const [search, setSearch] = React.useState('');
+    const handleReset = () => {
+        setCountries([]);
+    };
+
 
     async function fetchCountry() {
         try {
@@ -19,54 +23,54 @@ function App() {
         }
     }
 
+    async function searchCountry() {
+        try {
+            const searchResult = await axios.get('https://restcountries.com/v3.1/name/{name}');
+            setSearch(searchResult.name.common)
+        }
+    }
+
     return (
-               <>
+        <>
             <main>
                 <div className="header">
-                <img src={worldmap} alt="map of the world" className="worldMap"/>
-                <h1>World Regions</h1>
-                <button type='button' onClick={() => fetchCountry()}>Zoek Land</button>
+                    <img src={worldmap} alt="map of the world" className="worldMap"/>
+                    <h1>World Regions</h1>
+                    <button type='button' onClick={() => fetchCountry()}>Zoek Land</button>
+                    <button type='reset' onClick={handleReset}>Reset</button>
                 </div>
 
                 <div className="box">
-                {countries.length > 0 &&
-                    countries
-                        .sort((a, b) => a.population - b.population)
-                        .map((country) => {
-                    return <li key={country.name.common} className='countryInfo'>
-                        <CountryTile
-                            img={country.flags.svg}
-                            alt={"country flag"}
-                            name={country.name.common}
-                            className={color(country.region)}
-                            population={country.population}
-                        />
-                    </li>
-                        })
+                    {countries.length > 0 &&
+                        countries
+                            .sort((a, b) => a.population - b.population)
+                            .map((country) => {
+                                return <li key={country.name.common} className='countryInfo'>
+                                    <CountryTile
+                                        img={country.flags.svg}
+                                        alt={"country flag"}
+                                        name={country.name.common}
+                                        className={color(country.region)}
+                                        population={country.population}
+                                    />
+                                </li>
+                            })
                     }
+                </div>
+
+                <div>
+                    <h2>Search country information</h2>
+                    <div className='searchbar'>
+                        <input type="text" placeholder="Bijvoorbeeld Nederland of Peru" className='searchField' on/>
+                        <button type='button' className='searchButton'>ZOEK</button>
+                    </div>
                 </div>
 
 
             </main>
         </>
 
-        // <>
-        //     <main>
-        //         <img src={worldmap} alt="map of the world "/>
-        //         <h1>World Regions</h1>
-        //         <button type='button' onClick={() => fetchCountry()}>Zoek Land</button>
-        //         {countries.length > 0 && countries.map((country) => {
-        //             return (
-        //                 <li key={country[0].name} className='countryInfo'>
-        //                     <img height="30" width="50" src={country.flags.svg} alt="country flag"/>
-        //                     <p>{country[0].name}</p>
-        //                     <p>"Has a population of " {country[0].population}</p>
-        //                 </li>
-        //             )
-        //         })
-        //         }
-        //     </main>
-        // </>
+
     )
 }
 
